4eb6ad1109868f1f61ddb02a915cf4bc529e7ab9
MUHAMMAD ROFIQ RIDHANI <543241135@smktelkom-bjb.sch.id>
2026-06-10 09:30:22 +0800

    commit 2

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -7,6 +7,37 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+
+    public function Register(Request $request) {
+        $$request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:id',
+            'password'=> 'required|min:6',
+        ]);
+
+        $user = $request->user();
+
+         $token = $user->createToke('auth-token')->plainTextContent;
+
+        User::create([
+            'name' => $request->name,
+            'email'=> $request->email,
+            'password'=> bcrypt($request->password),
+        ]);
+
+        return response()->json([
+            'status'=> 'success',
+            'message'=> 'Registration successful',
+            'data' => [
+                'id' => $user->id,
+                'name'=> $user->name,
+                'email'=> $user->email,
+                'created_at' => $user->created_at,
+                'updated_at'=> $user->updated_at,
+                'token' => $token
+            ]
+        ]);
+    }
     public function login(Request $request){
         $data =  $request->validate([
             'email' => 'required',
@@ -29,11 +60,22 @@ class AuthController extends Controller
         'message'=> 'Login successful',
         'data' => [
              'id' => $user->id,
+             'name' => $user->name,
+             'email'=> $user->email,
+             'created_at' => $user->created_at,
+             'updated_at'=> $user->updated_at,
+             'token'=> $token
         ]
     ]);
 }
 
-  public function logout(){
+  public function logout(Request $request){
+    $user = $request->user();
+    $user->currentAccessToken()->delete();
 
+    return response()->json([
+      'status' => 'success',
+      'message'=> 'Logout successful'
+    ], );
   }
 }
